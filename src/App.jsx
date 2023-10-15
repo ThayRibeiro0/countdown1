1d9c0e21349ff65fe5fe7701c81a2dfcf8f0a07f
Thays Ribeiro <94051605+ThayRibeiro0@users.noreply.github.com>
2023-10-15 15:34:56 -0700

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
+import Title from './components/Title'
+import Counter from './components/Counter'
+
+import Cute from './assets/cute.jpg'
+
+import useCountdown from './hooks/useCountdown'
+
+
 
 import './App.css'
 
 function App() {
-  
+  const [day, hour, minute, second] = useCountdown("December 16, 2023 00:00:00")
 
   return (
-    <div className='App'>
-      <h1>Countdown</h1>
+    <div className='App' style={{backgroundImage: `url(${Cute})`}}>
+      <div className='container'>
+        <Title title="🧔🏼‍♂️❤️👩🏾‍🦱 Mr.& Mrs. Maher"/>
+        <Title title="💍 Wedding Day 💒💕"/>
+        <div className="countdown-container">
+          <Counter title="Days" number={day}/>
+          <Counter title="Hours" number={hour}/>
+          <Counter title="Minutes" number={minute}/>
+          <Counter title="Seconds" number={second}/>
+        </div>
+      </div>
     </div>
   )
 }
